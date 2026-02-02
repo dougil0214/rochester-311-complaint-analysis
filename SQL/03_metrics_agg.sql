b54f54b0bffa0f238d9456f0c4b9de4d1d46ade3
dougil0214 <dougil0214@gmail.com>
2026-02-02 16:14:25 -0500

    Added quieries on outlier columns

--- SQL/03_metrics_agg.sql
+++ SQL/03_metrics_agg.sql
@@ -16,7 +16,8 @@ FROM complaints_clean
 GROUP BY request_month
 ORDER BY request_month;
 
--- Complaints per street
+-- Complaints per street name
+
 SELECT 
 	COUNT(complaint_type) AS complaint_count,
     street_name
@@ -25,3 +26,52 @@ WHERE street_name NOT LIKE '% and %'
 AND street_name NOT LIKE '% & %' -- Exlcluding data where street names are labeled as 'intersection of steet and street'
 GROUP BY street_name
 ORDER BY street_name;
+
+-- Top 10 streets and % of total complaints
+
+-- CTE
+WITH street_complaints AS( SELECT 
+	COUNT(complaint_type) AS complaint_count,
+    street_name
+FROM complaints_clean
+WHERE street_name NOT LIKE '% and %' -- Exlcluding data where street names are labeled as 'intersection of steet and street'
+AND street_name NOT LIKE '% & %'-- Exlcluding data where street names are labeled as 'intersection of steet & street'
+AND street_name NOT LIKE 'unknown' -- Excluding data with street name as 'unknown'
+GROUP BY street_name
+ORDER BY street_name)
+
+SELECT 
+*,
+complaint_count/SUM(complaint_count) OVER() AS percentage_of_total_complaints
+FROM street_complaints
+ORDER BY complaint_count DESC
+LIMIT 10;
+
+-- Quantify unkown street name
+
+SELECT 
+	complaint_type,
+	COUNT(complaint_type) AS count_of_complaint_type
+FROM complaints_clean
+WHERE street_name LIKE 'unkn%'
+GROUP BY complaint_type
+ORDER BY count_of_complaint_type DESC;
+
+-- Quantify 'intersection of steet and (&) street'
+
+SELECT 
+	complaint_type,
+	COUNT(complaint_type) AS count_of_complaint_type
+FROM complaints_clean
+WHERE street_name LIKE '% and %'
+OR street_name LIKE '% & %'
+GROUP BY complaint_type
+ORDER BY count_of_complaint_type DESC;
+
+
+
+
+
+
+
+
